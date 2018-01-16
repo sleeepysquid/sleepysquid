6ccf9710828e9ca9c8045f6afa16a7fa55032542
Lawrence Bass <lawmbass1@gmail.com>
2018-01-16 17:45:48 -0500

    Add placeholder brand image

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,18 @@
 import React from "react"
 import Helmet from "react-helmet"
 
+import backgroundUrl from "../assets/sleepysquid_banner.png"
+
+
+const divStyle = {
+    height: '100vh',
+    backgroundImage: 'url(' + backgroundUrl + ')',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    zIndex: 999
+};
+
+
 export default ({ children }) => (
     <div>
         <Helmet defaultTitle={`SleepySquid`} titleTemplate={`%s | SleepySquid`}>
@@ -9,8 +21,10 @@ export default ({ children }) => (
           <meta name="og:site_name" content="SleepySquid" />
           <html lang="en" />
         </Helmet>
-        <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+        {/* <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
             {children()}
+        </div> */}
+        <div style={divStyle}>
         </div>
     </div>
 );
