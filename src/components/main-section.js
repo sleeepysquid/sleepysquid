1e7a9d99eb0166d2421ffd71f5f8bd57540cc801
Lawrence Bass <lawmbass1@gmail.com>
2018-01-20 08:53:57 -0500

    Hide unused pages

--- src/components/main-section.js
+++ src/components/main-section.js
@@ -4,7 +4,7 @@ import { Flex, Box } from "grid-styled"
 
 export default ({ children }) => (
   <Flex>
-    <Box width={2/3} mx='auto' my='5em'>
+    <Box width={1/2} mx='auto' my='5em'>
       {children}
     </Box>
   </Flex>
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -52,9 +52,9 @@ export default () => {
                             style={{ height: rhythm(5), width: rhythm(10)}} />
                     </Link>
                 </li>
-                <NavItem linkTo="/services">Services</NavItem>
+                {/* <NavItem linkTo="/services">Services</NavItem>
                 <NavItem linkTo="/tutorials">Tutorials</NavItem>
-                <NavItem linkTo="/blog">Blog</NavItem>
+                <NavItem linkTo="/blog">Blog</NavItem> */}
                 <li style={{display: `inline-block`, margin: `20px`}}>
                     <a href="https://github.com/sleeepysquid" title="GitHub">
                         <GithubIcon style={{ verticalAlign: `center` }} />
